48942dff3702d206c4b2544127a745dee9aab6fb
NguyenKhanhDuy-vie <songcancodamme@gmail.com>
2025-07-24 14:35:08 +0700

    done

---
+++ project1/MDK-ARM/test_doan1/port.d
@@ -0,0 +1,12 @@
+test_doan1\port.o: ../Middlewares/Third_Party/FreeRTOS/Source/portable/RVDS/ARM_CM3/port.c
+test_doan1\port.o: ../Middlewares/Third_Party/FreeRTOS/Source/include/FreeRTOS.h
+test_doan1\port.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stddef.h
+test_doan1\port.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
+test_doan1\port.o: ../Core/Inc/FreeRTOSConfig.h
+test_doan1\port.o: ../Middlewares/Third_Party/FreeRTOS/Source/include/projdefs.h
+test_doan1\port.o: ../Middlewares/Third_Party/FreeRTOS/Source/include/portable.h
+test_doan1\port.o: ../Middlewares/Third_Party/FreeRTOS/Source/include/deprecated_definitions.h
+test_doan1\port.o: ../Middlewares/Third_Party/FreeRTOS/Source/portable/RVDS/ARM_CM3/portmacro.h
+test_doan1\port.o: ../Middlewares/Third_Party/FreeRTOS/Source/include/mpu_wrappers.h
+test_doan1\port.o: ../Middlewares/Third_Party/FreeRTOS/Source/include/task.h
+test_doan1\port.o: ../Middlewares/Third_Party/FreeRTOS/Source/include/list.h
---
+++ project1/MDK-ARM/test_doan1/tasks.d
@@ -0,0 +1,16 @@
+test_doan1\tasks.o: ../Middlewares/Third_Party/FreeRTOS/Source/tasks.c
+test_doan1\tasks.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdlib.h
+test_doan1\tasks.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\string.h
+test_doan1\tasks.o: ../Middlewares/Third_Party/FreeRTOS/Source/include/FreeRTOS.h
+test_doan1\tasks.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stddef.h
+test_doan1\tasks.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
+test_doan1\tasks.o: ../Core/Inc/FreeRTOSConfig.h
+test_doan1\tasks.o: ../Middlewares/Third_Party/FreeRTOS/Source/include/projdefs.h
+test_doan1\tasks.o: ../Middlewares/Third_Party/FreeRTOS/Source/include/portable.h
+test_doan1\tasks.o: ../Middlewares/Third_Party/FreeRTOS/Source/include/deprecated_definitions.h
+test_doan1\tasks.o: ../Middlewares/Third_Party/FreeRTOS/Source/portable/RVDS/ARM_CM3/portmacro.h
+test_doan1\tasks.o: ../Middlewares/Third_Party/FreeRTOS/Source/include/mpu_wrappers.h
+test_doan1\tasks.o: ../Middlewares/Third_Party/FreeRTOS/Source/include/task.h
+test_doan1\tasks.o: ../Middlewares/Third_Party/FreeRTOS/Source/include/list.h
+test_doan1\tasks.o: ../Middlewares/Third_Party/FreeRTOS/Source/include/timers.h
+test_doan1\tasks.o: ../Middlewares/Third_Party/FreeRTOS/Source/include/stack_macros.h
